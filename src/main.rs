9e0d3c4d2fa1de45e3a834912c4942e3df2ef27c
leihang <leihang@mchz.com.cn>
2023-04-16 08:38:09 +0800

    OLED

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,107 @@
-#![deny(unsafe_code)]
+// Draw a 1 bit per pixel black and white image. On a 128x64 SSD1306 display over I2C.
+//
+// Image was created with ImageMagick:
+//
+// ```bash
+// convert rust.png -depth 1 gray:rust.raw
+// ```
+//
+// This example is for the STM32F103 "Blue Pill" board using I2C1.
+//
+// Wiring connections are as follows for a CRIUS-branded display:
+//
+// ```
+//      Display -> Blue Pill
+// (black)  GND -> GND
+// (red)    +5V -> VCC
+// (yellow) SDA -> PB9
+// (green)  SCL -> PB8
+// ```
+//
+
 #![no_std]
 #![no_main]
-
+// GND 电源地
+// VCC 电源正（3～5.5V）
+// SCL OLED的D0脚，在IIC通信中为时钟管脚
+// SDA OLED的D1脚，在IIC通信中为数据管脚
 use panic_halt as _;
 use nb::block;
-use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use cortex_m_rt::{
+    entry,
+    exception,
+    ExceptionFrame,
+};
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    pixelcolor::BinaryColor,
+    prelude::*,
+};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+
+use stm32f1xx_hal::{
+    pac,
+    prelude::*,
+    timer::Timer,
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    stm32
+};
+
 
 #[entry]
 fn main() -> ! {
-    // Get access to the core peripherals from the cortex-m crate
     let cp = cortex_m::Peripherals::take().unwrap();
-    // Get access to the device specific peripherals from the peripheral access crate
-    let dp = pac::Peripherals::take().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
     let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    // Acquire the GPIOC peripheral
-    // let mut gpioc = dp.GPIOC.split();
-    let mut gpioa = dp.GPIOA.split();
+    let mut afio = dp.AFIO.constrain();
 
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioa.pa1.into_push_pull_output(&mut gpioa.crl);
-    // let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
+    let mut gpiob = dp.GPIOB.split();
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400.kHz(),
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./a.raw"), 128);
+    let raw2: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./b.raw"), 128);
+
+    let im = Image::new(&raw, Point::new(0, 0));
+    let im2 = Image::new(&raw2, Point::new(0, 0));
+
+    
     let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(10.Hz()).unwrap();
+    timer.start(5.Hz()).unwrap();
 
-    // Wait for the timer to trigger an update and change the state of the LED
     loop {
+        im.draw(&mut display).unwrap();
+        display.flush().unwrap();
         block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
+        display.flush().unwrap();
+        im2.draw(&mut display).unwrap();
+        display.flush().unwrap();
     }
 }
+
